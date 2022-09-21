d39d74cd7340c1eeb8f2077223c50459d8d6ff05
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-09-21 23:59:49 +0300

    init no routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { BrowserRouter } from 'react-router-dom';
-import Header from './modules/Header';
-import ClientRoutes from './Client-routes';
+
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <ClientRoutes />
-    </BrowserRouter>
+   <div></div>
   );
 };
--- src/pages/ProductsPage/ProductsPage.jsx
+++
@@ -1,5 +0,0 @@
-const HomePage = () => {
-  return <h2>Products</h2>;
-};
-
-export default HomePage;
